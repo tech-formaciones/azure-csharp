52c8f852afc93143dcfc2209f03c5beaf3909f07
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-06-24 10:57:56 +0200

    Commit 2, día 24-06-2024

--- Formacion.Azure.CosmoDB.ConsoleApp1/Program.cs
+++ Formacion.Azure.CosmoDB.ConsoleApp1/Program.cs
@@ -4,8 +4,8 @@ namespace Formacion.Azure.CosmoDB.ConsoleApp1
 {
     internal class Program
     {
-        static readonly string endpointCosmosDB = "https://demodbbcr.documents.azure.com:443/";
-        static readonly string keyCosmosDB = "raCAln1cL6SdeEiHEUkQGPiAm1Pu4VOyHXDWMmRFcQ79sqmii2m0FeHEoSOfPPoD8LMKgQKlXlzTACDbmUHSgA==";
+        static readonly string endpointCosmosDB = "";
+        static readonly string keyCosmosDB = "==";
 
         static CosmosClient clientCosmosDB;
 
@@ -14,7 +14,8 @@ namespace Formacion.Azure.CosmoDB.ConsoleApp1
             clientCosmosDB = new CosmosClient(endpointCosmosDB, keyCosmosDB);
 
             Console.Clear();
-            GetDatabases();
+            //GetDatabases();
+            CreateRecord("DemoDB", "productos");
         }
 
         static void GetDatabases()
@@ -47,5 +48,49 @@ namespace Formacion.Azure.CosmoDB.ConsoleApp1
                 }
             }
         }
+
+        static void CreateRecord(string databaseName, string containerName)
+        {
+            var clientDatabase = clientCosmosDB.GetDatabase(databaseName);
+            var clientContainer = clientDatabase.GetContainer(containerName);
+
+            ///////////////////////////////////////////////////////
+            // Ejemplo con CLASS
+            ///////////////////////////////////////////////////////
+            var producto = new Producto()
+            { 
+                id = "10",
+                referencia = "10",
+                categoria = "Bebidas",
+                descripción = "Bebida de Naranja 33 cl",
+                cantidad = 5,
+                precio = 1.85
+            };
+
+            var result = clientContainer.CreateItemAsync(producto, new PartitionKey("Bebidas")).Result;
+
+            Console.WriteLine($"Producto creado con ID {result.Resource.id}.");
+
+            ///////////////////////////////////////////////////////
+            // Ejemplo con RECORD
+            ///////////////////////////////////////////////////////
+            var producto2 = new Product("11", "11", "Bebidas", "Refresco de Cola 1L.", 26, 2.80);
+            
+            var result2 = clientContainer.CreateItemAsync(producto2, new PartitionKey("Bebidas")).Result;
+            Console.WriteLine($"Producto creado con ID {result2.Resource.id}.");
+        }
     }
+
+
+    public class Producto
+    { 
+        public string id { get; set; }
+        public string referencia { get; set; }
+        public string categoria { get; set; }
+        public string descripción { get; set; }
+        public int cantidad { get; set; }
+        public double precio { get; set; }
+    }
+
+    public record Product(string id, string referencia, string categoria, string descripción, int cantidad, double precio);
 }
